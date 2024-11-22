87176c18f93da9cc6a99cede4853e4e935367b8c
saloni11042 <saloni11042@gmail.com>
2024-11-22 10:51:14 +0530

    api updated

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const users = require('./MOCK_DATA.json')
 const app = express();
 
-app.get('/users',(req,res)=>{
+app.get('/api/users',(req,res)=>{
     return res.json(users)
+})
+
+app.get('/users',(req,res)=>{
+    const html =`
+    <ul>${users.map((user)=>`<li>${user.id}</li>`)}</ul>`
+    return res.send(html)
+})
+
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number.parseInt(req.params.id);
+    const user = users.find((user)=>user.id===id)
+    return res.json(user)
 })
 
 app.listen(3000,()=>{
